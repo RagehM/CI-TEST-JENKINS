eead95570a72b1b7427dfcf0c1ceb68993f1d523
VNET\mrageh <mohamed.rageh.ext@valeo.com>
2025-08-19 12:06:33 +0300

    feat: uploaded code with unit testing for POC

---
+++ CI_Test/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/CI_Test/Tests/CI_Test1.bwt#/"/>
+  <EmulationDataFile>ACED000574000C43495F54657374312E627774</EmulationDataFile>
+  <EmulationDataFile>ACED000574000C43495F54657374322E627774</EmulationDataFile>
+</emulation:BWTFile>
